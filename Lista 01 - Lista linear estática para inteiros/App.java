93caa4ae4c450e952027bb785c5c7e30d0d0d63b
Amanda Cristina Suave Vieira <91279602+amandiinhaa@users.noreply.github.com>
2026-03-16 20:51:01 -0300

    Add files via upload

---
+++ Lista 01 - Lista linear estática para inteiros/App.java
@@ -0,0 +1,10 @@
+package AlgoritmosEstruturaDados.AlgoritmosEstruturaDados;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- Lista 01 - Lista linear estática para inteiros/ListaEstatica.java
+++ Lista 01 - Lista linear estática para inteiros/ListaEstatica.java
@@ -1,1 +1,83 @@
+package L01;
 
+public class ListaEstatica {
+
+	private int[] info;
+	private int tamanho;
+	
+	public ListaEstatica() {
+		super();
+		this.info = new int[10];
+		this.tamanho = 0;
+	}
+	
+	public void inserir(int valor) {
+		if (this.info.length == this.tamanho) {
+			this.redimensionar();
+		}
+		this.info[this.tamanho] = valor;
+		this.tamanho ++;
+	}
+	
+	private void redimensionar() {
+		int novovetor[];
+		novovetor = new int[this.info.length + 10];
+		for(int i = 0; i < this.tamanho; i++) {
+			novovetor[i] = this.info[i];
+		}
+		this.info = novovetor;
+	}
+	
+	public int buscar(int valor) {
+		for(int i = 0; i < this.tamanho; i++) {
+			if(this.info[i] == valor) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public boolean isVazia() {
+		if(this.tamanho == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void retirar(int valor) {
+		int pos = this.buscar(valor);
+		if(pos != -1) {
+			for(int i = pos; i < this.tamanho-1; i++) {
+				this.info[i] = this.info[i+1];
+			}
+			this.tamanho--;
+		}
+	}
+	
+	public void exibir() {
+		for(int i = 0; i < this.tamanho; i++) {
+			System.out.println(this.info[i]);
+		}
+	}
+	
+	public String toString() {
+		String s = " [ ";
+		for(int i = 0; i < this.tamanho-1; i ++) {
+			s = s + this.info[i] + ", ";
+		}
+		if(tamanho > 0) {
+			s  = s + this.info[tamanho-1] + " ] ";
+		}
+		return s;
+	}
+	
+	public void liberar() {
+		this.info = new int[10];
+		this.tamanho = 0;
+	}
+	
+	public int getTamanho() {
+		return this.tamanho;
+	}
+}
