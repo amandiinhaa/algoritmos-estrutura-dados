5bf532cf7116551d74cded0f4b20db28822744a3
Amanda Cristina Suave Vieira <91279602+amandiinhaa@users.noreply.github.com>
2026-06-08 20:31:28 -0300

    Create BuscaAbstract.java

---
+++ Lista 11 - Buscas/Q1Q2/BuscaAbstract.java
@@ -0,0 +1,14 @@
+package L11.Q1Q2;
+
+public abstract class BuscaAbstract<T> {
+
+    private Object[] info;
+
+    public Object[] getInfo() {
+        return info;
+    }
+
+    public void setInfo(Object[] info) {
+        this.info = info;
+    }
+}
